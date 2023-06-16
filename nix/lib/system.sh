bd79e902a4f9a26dedf611f0a1f2891452d4e473
Gregor A. Segner <gregor.segner@gmail.com>
2023-06-16 12:07:39 +0200

    Adding documentation

--- nix/lib/system.sh
+++ nix/lib/system.sh
@@ -1,12 +1,73 @@
 #! /bin/bash
+#
+# -----
+# @brief   :
+# @author  : Gregor A. Segner
+# @license : BSD-3
+# @repo    :
+# @issues  :
+#
+# last modification: 2023-06-16
+#
+# @description:
+#	This is a little collection of various helper functions written in BASH,
+# 	which can be refferenced in other bash-scripts. The collection contains
+#   following functions:
+#		* getGitHubRelease
+# 		* getHostArch
+#
+#	A detailed description for each function can be found by the function 
+#   itself.
+#
+# @dependencies:
+#   system.sh has no additinal dependencies to other bash-scripts.
+#
+# @install:
+# 	It is recommended to place system.sh as a system library in
+# 		$HOME/.local/share/bash-lib/sytem.sh
+#   Also it is recommended, that your scripts check the existence of system.sh
+#   before using them.
+# -----
 
 
-## IN: username/repo
+###################
+# Fetch latest release tag from github repo, so it' s possible
+# to simply fetch this release with something like curl.
+#
+# getGitHubRelease() requires jq installed on system.
+#
+# Globals:
+#	None
+#
+# Arguments:
+#	repo (string) - the form is <username>/<reponame>
+#
+# Outputs:
+#	stdout - the release-tag
+#
+# Returns
+# 	None
+###################
 function getGitHubRelease () {
 	echo=$(curl -sL "https://api.github.com/repos/${1}/releases/latest" | jq -r ".tag_name")
 }
 
 
+###################
+# get the architecture of the system where scripts are executed
+#
+# Globals:
+#	None
+#
+# Arguments:
+# 	None
+#
+# Outputs:
+# 	stdout - the arch of the system
+#
+# Returns:
+# 	None
+###################
 function getHostArch() {
 	arch=$(lscpu | grep Architecture | cut -d ':' -f 2 | xargs)
 	if [[ -z arch ]]; then

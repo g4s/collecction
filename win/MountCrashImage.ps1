e3f70275026eb30e0a11b30d2d522fee1e66bc2a
Gregor A. Segner <gregor.segner@gmail.com>
2023-06-16 11:19:59 +0200

    make MountCrashimage more generic

--- win/MountCrashImage.ps1
+++ win/MountCrashImage.ps1
@@ -1,2 +1,24 @@
+# -----
+# @brief   : script for mounting a crash-image vhd on windows
+# @author  : Gregor A. Segnr
+# @license : BSD-3
+#
+# last modification: 2023-06-16
+# -----
+#
+
+$startdir = (Get-Location).Path
+
 Mount-DiskImage -ImagePath "D:\CrashImage.vhd"
-Start-Process "E:\WinTools\SyMenu.exe"
+
+$volume = (Get-Volume -FileSystemLabel Ventoy).DriveLetter
+$startexe = $volume + ':\WinTools\SyMenu.exe'
+Start-Process $startexe
+
+$scriptdir = $volume + ':\scripts'
+Set-Location -Path $scriptdir
+git config --global --add safe.directory $scriptdir
+git pull
+
+Set-Location -Path $startdir
+clear

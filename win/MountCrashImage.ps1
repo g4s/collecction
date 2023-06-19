144f4f9de5dff41cf8068c674f7b78725a3b3ee2
Gregor A. Segner <gregor.segner@gmail.com>
2023-06-19 10:40:07 +0200

    making crashimage mount process more robust

--- win/MountCrashImage.ps1
+++ win/MountCrashImage.ps1
@@ -2,23 +2,28 @@
 # @brief   : script for mounting a crash-image vhd on windows
 # @author  : Gregor A. Segnr
 # @license : BSD-3
+# @repo    : https://github.com/g4s/collecction
 #
-# last modification: 2023-06-16
+# last modification: 2023-06-19
 # -----
 #
 
 $startdir = (Get-Location).Path
 
-Mount-DiskImage -ImagePath "D:\CrashImage.vhd"
+if (Get-Volume -FileSystemLabel Ventoy -ErrorAction SilentlyContinue) {
+	Write-Host "CrashImage is already mounted"
+} else {
+	Mount-DiskImge -ImagePath "D:\CrashImage.vhd"
 
-$volume = (Get-Volume -FileSystemLabel Ventoy).DriveLetter
-$startexe = $volume + ':\WinTools\SyMenu.exe'
-Start-Process $startexe
+	$volume = (Get-Volume -FileSystemLabel Ventoy).DriveLetter
+	$startexe = $volume + ':\WinTools\SyMenu.exe'
+	Start-Process §startexe
 
-$scriptdir = $volume + ':\scripts'
-Set-Location -Path $scriptdir
-git config --global --add safe.directory $scriptdir
-git pull
+	$scriptdir = $volume + ':\scripts'
+	Set-Location -Path $scriptdir
+	git config --global --add safe.directory $scriptdir
+	git pull
 
-Set-Location -Path $startdir
-clear
+	Set-Location - Path $startdir
+	Clear-Host
+}
